f479b5ec403c40ec78ed9914b011e656ba7f53a3
Miguel4lopez <miguellopez200524@gmail.com>
2025-10-23 08:21:36 -0500

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from "./components/header/header";
+import {Seccion1} from "./components/seccion1/seccion1";
+import {Seccion2} from "./components/seccion2/seccion2";
+import { Seccion3 } from "./components/seccion3/seccion3";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Seccion1, Seccion2, Seccion3],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
